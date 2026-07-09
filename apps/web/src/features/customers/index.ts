940254d24439dadb38ea9275142cd25df5d4b39a
Ebrahim Ghaffari <evrin361@gmail.com>
2026-07-09 22:45:30 +0330

    refactor: establish customer feature public api boundary

--- apps/web/src/features/customers/index.ts
+++ apps/web/src/features/customers/index.ts
@@ -1,1 +1,13 @@
 export { CustomersPage } from "./pages/CustomersPage";
+
+export { useCustomers } from "./hooks/useCustomers";
+
+export { customerService } from "./services/customer.service.instance";
+
+export type {
+  Customer,
+} from "./types/customer";
+
+export type {
+  CreateCustomerDto,
+} from "./hooks/useCustomers";
